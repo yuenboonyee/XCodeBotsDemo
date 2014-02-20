d3a89fd77a1688c6f7613484b26ee48aefeb0f19
Yuen Boon Yee <yuen_boon_yee@sereneit.com>
2014-02-20 21:31:50 +1100

    Initial Commit

---
+++ XCodeBotsDemo/SITViewController.h
@@ -0,0 +1,13 @@
+//
+//  SITViewController.h
+//  XCodeBotsDemo
+//
+//  Created by Serene IT on 20/02/14.
+//  Copyright (c) 2014 Serene IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SITViewController : UIViewController
+
+@end
---
+++ XCodeBotsDemo/SITViewController.m
@@ -0,0 +1,29 @@
+//
+//  SITViewController.m
+//  XCodeBotsDemo
+//
+//  Created by Serene IT on 20/02/14.
+//  Copyright (c) 2014 Serene IT. All rights reserved.
+//
+
+#import "SITViewController.h"
+
+@interface SITViewController ()
+
+@end
+
+@implementation SITViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
